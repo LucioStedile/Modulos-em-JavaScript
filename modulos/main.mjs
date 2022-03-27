6c307957ece26bc880f62148e58556c36e4dd8a1
LucioStedile <https://github.com/LucioStedile>
2022-03-27 16:37:38 -0300

    Aula de módulos em javascript

---
+++ modulos/main.mjs
@@ -0,0 +1,5 @@
+import { mostraCidade, mostraIdade, mostraHobby } from "./funcoes.mjs";
+
+console.log(mostraIdade("Lúcio", 51));
+console.log(mostraCidade("Lúcio", "Fortaleza"));
+console.log(mostraHobby("Lúcio", "pescar"));
